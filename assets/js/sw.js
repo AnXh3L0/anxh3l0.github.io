636030a6b35e0f0df0429b2af714754613acf920
Anxhelo Lushka <anxhelo1995@gmail.com>
2018-09-12 01:12:41 +0200

    Service Worker install and fetch added

---
+++ assets/js/sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'lushka-al-cache';
+var urlsToCache = [
+  '/',
+  '/assets/main.css',
+  '/fonts/overpass.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
