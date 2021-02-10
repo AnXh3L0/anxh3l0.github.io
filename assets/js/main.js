b14fa9a3c1137493e14716bf7493046d756f4e3f
Anxhelo Lushka <anxhelo1995@gmail.com>
2021-02-10 18:36:49 +0100

    Moved embeds logic to the main JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,91 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("hireme").style.display = "none";
   });
 });
+
+// Embeds
+
+function embedIframe(div) {
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute(
+    "src",
+    "https://www.youtube-nocookie.com/embed/" +
+      div.dataset.id +
+      "?autoplay=1&rel=0"
+  );
+  iframe.setAttribute("title", div.dataset.title);
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("allowfullscreen", "1");
+  iframe.setAttribute("modestbranding", "1");
+  iframe.setAttribute(
+    "allow",
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  );
+  div.parentNode.replaceChild(iframe, div);
+}
+
+function initYouTubeVideos() {
+  var playerElements = document.getElementsByClassName("youtube-player");
+  for (var n = 0; n < playerElements.length; n++) {
+    var videoId = playerElements[n].dataset.id;
+    var videoTitle = playerElements[n].dataset.title;
+    var div = document.createElement("div");
+    div.setAttribute("data-id", videoId);
+    var thumbNode = document.createElement("img");
+    thumbNode.alt = videoTitle;
+    thumbNode.src = "//i.ytimg.com/vi/ID/hqdefault.jpg".replace("ID", videoId);
+    div.appendChild(thumbNode);
+    var playButton = document.createElement("div");
+    playButton.setAttribute("class", "play");
+    div.appendChild(playButton);
+    div.onclick = function () {
+      embedIframe(this);
+    };
+    playerElements[n].appendChild(div);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", initYouTubeVideos);
+
+function embedVideo(div) {
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute(
+    "src",
+    "https://" +
+      div.dataset.domain +
+      "/playback/presentation/2.0/playback.html" +
+      "?meetingId=" +
+      div.dataset.id +
+      "&t=" +
+      div.dataset.timestamp
+  );
+  div.parentNode.replaceChild(iframe, div);
+}
+
+function initBBBVideos() {
+  var playerElements = document.getElementsByClassName("player");
+  for (var n = 0; n < playerElements.length; n++) {
+    var domain = playerElements[n].dataset.domain;
+    var videoId = playerElements[n].dataset.id;
+    var videoTitle = playerElements[n].dataset.title;
+    var thumbImage = playerElements[n].dataset.thumb;
+    var timestamp = playerElements[n].dataset.timestamp;
+    var div = document.createElement("div");
+    div.setAttribute("data-id", videoId);
+    div.setAttribute("data-domain", domain);
+    div.setAttribute("data-thumb", thumbImage);
+    div.setAttribute("data-timestamp", timestamp);
+    var thumbNode = document.createElement("img");
+    thumbNode.alt = videoTitle;
+    thumbNode.src = "/img/thumbs/image".replace("image", thumbImage);
+    div.appendChild(thumbNode);
+    var playButton = document.createElement("div");
+    playButton.setAttribute("class", "play");
+    div.appendChild(playButton);
+    div.onclick = function () {
+      embedVideo(this);
+    };
+    playerElements[n].appendChild(div);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", initBBBVideos);
